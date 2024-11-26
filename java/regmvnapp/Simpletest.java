c884a18e1dccd2bf3f414c4bb918ac366e7dd001
akhil0630 <akhilbethi04@gmail.com>
2024-11-25 22:07:06 -0800

    Add files via upload

---
+++ java/regmvnapp/Simpletest.java
@@ -0,0 +1,31 @@
+
+package regmvnapp;
+
+
+import org.openqa.selenium.WebDriver;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+public class Simpletest {
+
+@Test
+	
+	public void simple_Test()
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver= new ChromeDriver();
+	
+	driver.get("http://localhost:8084/regmvnapp");
+	driver.manage().window().maximize();
+	
+	
+	Assert.assertEquals(driver.getTitle(),"REGISTRATION EVENT");
+	driver.close();
+	}
+}
+
